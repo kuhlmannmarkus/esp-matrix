777e0f4039c750c61fa8221248cd660ac678dd59
Markus Kuhlmann <markus.kuhlmann@comma-soft.com>
2021-10-10 17:57:45 +0200

    Removing file headers

--- message.cpp
+++ message.cpp
@@ -1,9 +1,3 @@
-//
-// Copyright (c) 2021 Markus Kuhlmann - kuhlmann.markus@gmail.com
-//
-// Refer to the LICENSE file for licensing information
-//
-
 #include "message.h"
 
 Message::Message() {
--- message.h
+++ message.h
@@ -1,9 +1,3 @@
-//
-// Copyright (c) 2021 Markus Kuhlmann - kuhlmann.markus@gmail.com
-//
-// Refer to the LICENSE file for licensing information
-//
-
 #ifndef MESSAGE_H
 #define MESSAGE_H
 
